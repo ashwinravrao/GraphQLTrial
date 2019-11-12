20c4ed1a7b7375c2975b766bf24271392685a97f
Ashwin Rao <ashwinravrao@gmail.com>
2019-11-11 21:54:52 -0600

    Remove unnecessary dependencies

--- app/src/main/java/com/ashwinrao/graphqltrial/sl/Modules.kt
+++ app/src/main/java/com/ashwinrao/graphqltrial/sl/Modules.kt
@@ -1,25 +1,14 @@
 package com.ashwinrao.graphqltrial.sl
 
 import com.android.volley.toolbox.Volley
-import com.ashwinrao.graphqltrial.BuildConfig
 import com.ashwinrao.graphqltrial.network.DataSourceImpl
 import com.ashwinrao.graphqltrial.repository.RepositoryImpl
 import com.ashwinrao.graphqltrial.viewmodel.MainViewModel
-import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val networkModule = module {
-
-    single {
-        OkHttpClient.Builder().addNetworkInterceptor { chain ->
-            val request = chain.request().newBuilder()
-                .addHeader("Authorization", "bearer ${BuildConfig.GITHUB_OAUTH_TOKEN}")
-                .build()
-            chain.proceed(request)
-        }.build()
-    }
 
     single { Volley.newRequestQueue(androidContext()) }
 
